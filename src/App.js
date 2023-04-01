0757512e3a9a6b07fedcf459582a749c7e1d7d7b
Riteshgupta2906 <80892162+Riteshgupta2906@users.noreply.github.com>
2023-04-01 13:40:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Navbar from "./Component/header";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./Component/home";
+import CardExample from "./Component/corousel";
+// import Testimonial from "./Component/testimonial";
+import HigherStudiesCarousel from "./Component/HigherStudiesCarousel";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+      <div>
+        <Home />
+      </div>
+      <div>
+        <HigherStudiesCarousel />
+      </div>
+      <div>
+        <CardExample />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Slide.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { StackedCarouselSlideProps } from "react-stacked-center-carousel";
+import "./Slide.css";
+
+export const Slide = React.memo(function (StackedCarouselSlideProps) {
+  const { data, dataIndex, isCenterSlide, swipeTo, slideIndex } =
+    StackedCarouselSlideProps;
+
+  const coverImage = data[dataIndex].image;
+  const text = data[dataIndex].text;
+  const heading = data[dataIndex].heading;
+
+  console.log(coverImage);
+  return (
+    <div className="card-card" draggable={false}>
+      <div className={`cover fill ${isCenterSlide ? "off" : "on"}`}>
+        <div
+          className="card-overlay fill"
+          onClick={() => {
+            if (!isCenterSlide) swipeTo(slideIndex);
+          }}
+        />
+      </div>
+      <div className="detail fill">
+        <div className="discription">
+          <img
+            id="erin"
+            // style={{ width: 100 }}
+            alt="j"
+            className="cover-image"
+            src={coverImage}
+          />
+          <h4>{heading}</h4>
+          <p id="descriptionText">{text}</p>
+        </div>
+      </div>
+    </div>
+  );
+});
