256975fbfd6ce1ae456e343dd5c1ca1edc1cf5b5
AryanDhiman10 <97045861+AryanDhiman10@users.noreply.github.com>
2023-04-01 18:55:29 +0530

    Added footer and contact us link
    Added Footer and Contact Us
    Issues in Header.css, had to comment it out. Everything resolved after this.
    Someone has to redo the header work.

---
+++ src/Component/contact.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCol, MDBContainer, MDBIcon, MDBInput, MDBRow, MDBTypography, MDBTextArea } from 'mdb-react-ui-kit';
+import "./contact.css"
+
+export default function Image() {
+  return (
+    <MDBContainer className="py-5" style={{ maxWidth: '1200px' }}>
+      <MDBRow className="justify-content-center align-items-center">
+        <MDBCol>
+          <MDBCard className="my-4 shadow-3">
+            <MDBRow className="g-0">
+
+              <MDBCol md="6" className="d-xl-block bg-image">
+              <MDBCardBody className="p-md-5 text-black fw-light">
+                <MDBTypography tag="h3" className="mb-4 text-uppercase">CONTACT US</MDBTypography>
+                <MDBRow>
+                    <MDBCol md="6" className="mb-4"> 
+                        First Name
+                    </MDBCol>
+                    <MDBCol md="6" className="mb-4">
+                        Last Name
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                    <MDBCol md="6" className="mb-4"> 
+                        First Name
+                    </MDBCol>
+                    <MDBCol md="6" className="mb-4">
+                        Last Name
+                    </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+              </MDBCol>
+              
+              <MDBCol md="6">
+                <MDBCardBody className="p-md-5 text-black fw-light">
+                  
+
+                  <MDBRow>
+                    <MDBCol md="6" className="mb-4"> 
+                        First Name
+                      <MDBInput className="shadow-3" type='text' size="lg" />
+                    </MDBCol>
+                    <MDBCol md="6" className="mb-4">
+                        Last Name
+                      <MDBInput  type='text' size="lg" />
+                    </MDBCol>
+                  </MDBRow>
+                    Email
+                  <MDBInput  type='text' className="mb-4" size="lg" />
+                  Message
+                  <MDBTextArea  id='textAreaExample' rows={4} />
+
+                  <div className="d-flex justify-content-start pt-3">
+                    <MDBBtn size="lg" className="ms-2" style={{backgroundColor: 'hsl(210, 100%, 50%)'}}>Send</MDBBtn>
+                  </div>
+
+                </MDBCardBody>
+              </MDBCol>
+            </MDBRow>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
