a33ea1fbedf6255b23f1c28366fdfac4f138959c
AryanDhiman10 <97045861+AryanDhiman10@users.noreply.github.com>
2023-04-04 06:04:11 +0530

    Created Contact
    Created Contact from scratch and added it's functionality

---
+++ src/Component/contact/left.js
@@ -0,0 +1,36 @@
+export default function Left() {
+  return (
+    <>
+        <div className="header">
+          Contact Us
+        </div>
+        <div className="semiHeading">
+          Phone
+        </div>
+        <div className="Row">
+            <div className="Col"> 
+              Prof. Sai R. Meka <br></br>
+              Ass. Dean <br></br>
+              +91 1332 285378 <br></br>
+            </div>
+            <div className="Col"> 
+              Aman Vishwakarma <br></br>
+              Chairperson <br></br>
+              +91 9569802087 <br></br>
+            </div>
+        </div>
+        <div className="semiHeading">
+          Address
+        </div>
+        <div className="Row">
+            Ground Floor, Student Activity Center <br></br>
+            IIT Roorkee, Roorkee <br></br>
+            Uttarakhand, India - 247667
+        </div>
+        <div className="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3459.941259598978!2d77.89787421540963!3d29.865967833935947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390eb36c210c7d2b%3A0x43a317fe732508db!2sCIG%20IITR!5e0!3m2!1sen!2sin!4v1680548354157!5m2!1sen!2sin" 
+            title="CIG Office" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </>
+  );
+}
